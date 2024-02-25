afc8416fd1de885e14a60f93a54b724cad4e62c6
nvhiii <nahikhan16@gmail.com>
2024-02-25 18:14:34 -0500

    configuring intro to react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>React app test</h1>
+
+    </div>
+  );
+}
+
+export default App;
