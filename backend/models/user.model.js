3b2478bfb180a9b62a2429e139eaf4ba946e4634
nvhiii <nahikhan16@gmail.com>
2024-03-10 16:34:18 -0400

    Created user model in mongoDB

---
+++ backend/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true, // cannot have user w/o username
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true, // cannot have user w/o username
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true, // cannot have user w/o username
+    },
+  },
+  { timestamps: true } // each user will also have time of creation and time of edit
+);
+
+const User = mongoose.model(`User`, userSchema);
+
+export default User;
