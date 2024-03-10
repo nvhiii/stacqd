e433d39e8ae57d4f64cfcf4c108fd6bd013cf50c
nvhiii <nahikhan16@gmail.com>
2024-03-10 15:14:16 -0400

    installed more dependancies, cleared clutter, and included tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import Login from "./components/Login";
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+export default function App() {
+  return <h1 className="text-red-500 ">App</h1>;
 }
-
-export default App;
--- src/message.tsx
+++
@@ -1,8 +0,0 @@
-function Message() {
-
-    const name = `Nahi`;
-    return <h1>Hello {name}</h1>; // js html = jsx
-
-}
-
-export default Message;
