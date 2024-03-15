50ef1de722c07c9193d592edc93e77a714839f73
nvhiii <nahikhan16@gmail.com>
2024-03-15 11:07:32 -0400

    Added redux persist to implement loggedin

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 // root of app
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // provider for the global state
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
