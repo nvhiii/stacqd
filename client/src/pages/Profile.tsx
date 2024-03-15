453a65f09600142f435d6d5b69a20f8e1bce5c46
nvhiii <nahikhan16@gmail.com>
2024-03-15 16:04:54 -0400

    working on ui for profile

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,10 @@
-import React from "react";
-
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl font-semibold text-center my-7 bg-gradient-to-r from-blue-500 to-indigo-400 text-white">
+        Profile
+      </h1>
+      <form></form>
+    </div>
+  );
 }
