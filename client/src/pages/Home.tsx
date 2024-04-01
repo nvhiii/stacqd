2bd93ded8b28b8563ab2c06893e3aa8b8b2af6b7
nvhiii <nahikhan16@gmail.com>
2024-04-01 00:27:18 -0400

    updated home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">STACQD</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam odio
+        harum maiores dolorum quod aut esse sit ea cupiditate provident minus
+        quam in fugiat nam minima, earum reprehenderit alias nesciunt?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam odio
+        harum maiores dolorum quod aut esse sit ea cupiditate provident minus
+        quam in fugiat nam minima, earum reprehenderit alias nesciunt?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam odio
+        harum maiores dolorum quod aut esse sit ea cupiditate provident minus
+        quam in fugiat nam minima, earum reprehenderit alias nesciunt?
+      </p>
+    </div>
+  );
 }
